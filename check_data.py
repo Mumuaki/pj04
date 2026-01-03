c66c083e1546c4c06e43152da15a78a6eca46d45
Mumuaki <mumuaki@gmail.com>
2026-01-03 18:29:22 +0100

    feat: добавлена data-миграция для справочников узлов отказа и способов восстановления
    - Создана миграция 0003_add_initial_nodes_and_recovery_methods.py
    - обавлено 6 узлов отказа: вигатель, Трансмиссия, едущий мост, правляемый мост, идросистема, одъемное устройство
    - обавлено 2 способа восстановления: емонт узла, амена узла
    - обавлен вспомогательный скрипт check_data.py для проверки данных в справочниках

---
+++ check_data.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""Скрипт для проверки данных в базе данных"""
+import os
+import django
+
+# Настройка Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()
+
+from apps.service.models import FailureNode, RecoveryMethod
+
+print("=" * 50)
+print("УЗЛЫ ОТКАЗА (FailureNode)")
+print("=" * 50)
+nodes = FailureNode.objects.all()
+if nodes:
+    for node in nodes:
+        print(f"ID: {node.id:2d} | Название: {node.name}")
+else:
+    print("Нет данных")
+
+print("\n" + "=" * 50)
+print("СПОСОБЫ ВОССТАНОВЛЕНИЯ (RecoveryMethod)")
+print("=" * 50)
+methods = RecoveryMethod.objects.all()
+if methods:
+    for method in methods:
+        print(f"ID: {method.id:2d} | Название: {method.name}")
+else:
+    print("Нет данных")
+
+print("\n" + "=" * 50)
+print(f"Всего узлов отказа: {nodes.count()}")
+print(f"Всего способов восстановления: {methods.count()}")
+print("=" * 50)
