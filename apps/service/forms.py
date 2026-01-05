d0dbccbc7912461277443f6fb553f842ac4355cc
Mumuaki <151684806+Mumuaki@users.noreply.github.com>
2026-01-06 00:11:08 +0100

    forms.py

--- apps/service/forms.py
+++ apps/service/forms.py
@@ -13,16 +13,10 @@ class MachineForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-        # Фильтруем списки пользователей по ролям
         from apps.users.models import CustomUser
-        
-        # Поле "Клиент": только пользователи с ролью 'client'
         self.fields['client'].queryset = CustomUser.objects.filter(role='client')
-        
-        # Поле "Сервисная компания": только пользователи с ролью 'service'
         self.fields['service_company'].queryset = CustomUser.objects.filter(role='service')
 
-        # Можно добавить CSS классы для всех полей
         for field in self.fields:
             self.fields[field].widget.attrs.update({'class': 'form-input'})
 
@@ -39,41 +33,32 @@ class MaintenanceForm(forms.ModelForm):
         }
     
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)  # Получаем пользователя из kwargs
+        user = kwargs.pop('user', None)  
         super().__init__(*args, **kwargs)
         
-        # Фильтруем машины по пользователю
         if user:
             if getattr(user, 'is_manager', False) or user.is_superuser:
-                # Менеджер и суперпользователь видят все машины
                 pass
             elif getattr(user, 'is_service', False):
-                # Сервисная компания видит машины, где она указана как сервисная компания
                 self.fields['machine'].queryset = Machine.objects.filter(service_company=user)
             elif getattr(user, 'is_client', False):
-                # Клиент видит только свои машины
                 self.fields['machine'].queryset = Machine.objects.filter(client=user)
             else:
-                # Если роль не определена, не показываем машины
                 self.fields['machine'].queryset = Machine.objects.none()
         
-        # Фильтруем организации, проводившие ТО
         if user:
             from apps.users.models import CustomUser
             if getattr(user, 'is_manager', False) or user.is_superuser:
-                # Менеджер видит все сервисные компании
                 self.fields['service_company'].queryset = CustomUser.objects.filter(role='service')
             elif getattr(user, 'is_service', False):
-                # Сервисная компания видит только себя
                 self.fields['service_company'].queryset = CustomUser.objects.filter(id=user.id)
             elif getattr(user, 'is_client', False):
-                # Клиент видит себя и сервисные компании из своих машин
                 service_companies_ids = Machine.objects.filter(client=user).values_list('service_company_id', flat=True).distinct()
                 self.fields['service_company'].queryset = CustomUser.objects.filter(
                     id__in=list(service_companies_ids) + [user.id]
                 )
                 
-                # Переопределяем отображение (label) для текущего пользователя (клиента)
+                # Переопределено отображение (label) для текущего пользователя (клиента)
                 # Чтобы в списке вместо имени (например, "Клиент Иванов") отображалось "Самостоятельно"
                 original_label_from_instance = self.fields['service_company'].label_from_instance
 
@@ -95,9 +80,8 @@ class MaintenanceForm(forms.ModelForm):
         event_date = cleaned_data.get('event_date')
         service_type = cleaned_data.get('service_type')
         
-        # Проверяем на дубликаты (машина + дата + вид ТО)
+        # Проверка на дубликаты (машина + дата + вид ТО)
         if machine and event_date and service_type:
-            # Если это редактирование, исключаем текущую запись
             queryset = Maintenance.objects.filter(
                 machine=machine,
                 event_date=event_date,
@@ -124,25 +108,19 @@ class ComplaintForm(forms.ModelForm):
         }
     
     def __init__(self, *args, **kwargs):
-        user = kwargs.pop('user', None)  # Получаем пользователя из kwargs
+        user = kwargs.pop('user', None)  
         super().__init__(*args, **kwargs)
         
-        # Фильтруем машины по пользователю
         if user:
             if getattr(user, 'is_manager', False) or user.is_superuser:
-                # Менеджер и суперпользователь видят все машины
                 pass
             elif getattr(user, 'is_service', False):
-                # Сервисная компания видит машины, где она указана как сервисная компания
                 self.fields['machine'].queryset = Machine.objects.filter(service_company=user)
             elif getattr(user, 'is_client', False):
-                # Клиент видит только свои машины
                 self.fields['machine'].queryset = Machine.objects.filter(client=user)
             else:
-                # Если роль не определена, не показываем машины
                 self.fields['machine'].queryset = Machine.objects.none()
         
-        # Фильтруем сервисные компании
         if user:
             from apps.users.models import CustomUser
             if getattr(user, 'is_manager', False) or user.is_superuser:
